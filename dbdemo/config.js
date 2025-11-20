eaa115b3fb6419fa9b44ec2d76045f34c87861ab
Muskaan Gulrajani <muskaangulrajani@Muskaans-MacBook-Air.local>
2025-11-20 15:41:17 +0530

    changes

--- dbdemo/config.js
+++ dbdemo/config.js
@@ -4,10 +4,21 @@ dotenv.config();
 
 export const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
-    console.log('MongoDB connected...');
-  } catch (err) {
-    console.error(err.message);
+    const uri = process.env.MONGO_URI;
+    if (!uri) {
+      console.error("MONGO_URI is not defined!");
+      process.exit(1);
+    }
+    
+    mongoose.connect(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    })
+    .then(() => console.log("MongoDB connected"))
+    .catch(error => console.error("MongoDB connection error:", error)); 
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
     process.exit(1);
   }
 };
+    
